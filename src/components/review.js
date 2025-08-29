f02c27609e15ce6568feee90bbf06bdd46c28bf0
ElenaWikoff <elenawikoff@gmail.com>
2025-08-29 11:16:22 -0500

    Add homepage reviews

---
+++ src/components/review.js
@@ -0,0 +1,46 @@
+
+const ReviewCard = ({ quote, stars, name }) => {
+    const card = document.createElement("blockquote");
+    card.classList.add("review-card");
+
+    // Quote
+    const content = document.createElement("p");
+    content.textContent = quote;
+
+    // Stars
+    const rating = document.createElement("div");
+    rating.classList.add("rating");
+    for (let i = 0; i < 5; i++) {
+        const star = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        star.classList.add("star");
+        star.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        star.setAttribute("viewBox", "0 0 24 24");
+        if (i < stars) {
+            star.classList.add("filled");
+        }
+
+        // Path
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", "M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z");
+        path.setAttribute("fill", "currentColor");
+        path.setAttribute("width", "20px");
+        path.setAttribute("height", "20px");
+
+        star.appendChild(path);
+        rating.appendChild(star);
+    }
+
+    // Name
+    const nameElement = document.createElement("cite");
+    nameElement.classList.add("cite");
+    nameElement.textContent = "— " + name;
+
+    // Add Elements
+    card.appendChild(content);
+    card.appendChild(rating);
+    card.appendChild(nameElement);
+
+    return card;
+}
+
+export default ReviewCard;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,49 @@
+import ReviewCard from "../components/review.js";
+
+const reviews = [
+    {
+        quote: "Sweetest dining experience ever!",
+        stars: 5,
+        name: "Jerry Stinger"
+    },
+    {
+        quote: "A refreshing surprise—I'll bee back!",
+        stars: 4,
+        name: "B.J. Novak"
+    },
+    {
+        quote: "Their honey-butter biscuits were un-bee-lievable!",
+        stars: 5,
+        name: "Jana Beesley"
+    },
+    {
+        quote: "Amazing! The trenchcoats that kept on buzzing made for weird uniforms though.",
+        stars: 3,
+        name: "Thomas Combs"
+    },
+]
 
 const home = () => {
     const section = document.createElement("section");
     section.classList.add("container");
-    console.log(section);
-    section.textContent = "Home Page";
+
+    // Header
+    const heading = document.createElement("h2");
+    heading.textContent = "Where Every Bite is Golden."
+    heading.setAttribute("style", "font-style: italic");
+
+    // Reviews
+    const cardGrid = document.createElement("div");
+    cardGrid.classList.add("card-grid");
+    reviews.forEach(({ quote, stars, name }) => {
+        const reviewCard = ReviewCard({ quote, stars, name });
+        cardGrid.appendChild(reviewCard);
+    });
+
+    // Append Children to section
+    section.appendChild(heading);
+    section.appendChild(cardGrid);
+
     return section;
 }
 
